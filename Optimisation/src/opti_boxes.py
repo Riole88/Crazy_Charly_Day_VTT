186e373209dfbe7c3437d938d852751264680438
GabinM <gabin.mathieu54@gmail.com>
2026-02-19 11:40:34 +0100

    ajout classes de données

---
+++ Optimisation/src/opti_boxes.py
@@ -0,0 +1,36 @@
+class Child() :
+
+    def __init__(self, age : str, preferences : list[str]):
+        self.age = age
+        self.preferences = preferences
+
+    def belongsToAge(self, ageToCheck:str) -> bool:
+        return ageToCheck == self.age
+    
+class Toy():
+
+    def __init__(self, mass: int, price : int, category : str, age : str):
+        self.mass = mass
+        self.price = price
+        self.category = category
+        self.age = age
+
+    
+
+class Box() :
+
+    def __init__(self, child : Child, maximumMass : int):
+        self.toys = []
+        self.totalMass = 0
+        self.totalPrice = 0
+        self.childBelonging = child
+        self.maximumMass = maximumMass
+
+    def canAddToBox(self, toy : Toy) -> bool:
+        return toy.mass+self.totalMass <= self.totalMass and self.childBelonging.belongsToAge(toy.age)
+
+
+    def addToBox(self, toy : Toy) :
+        self.toys.append(toy)
+        self.totalMass += toy.mass
+        self.totalPrice += toy.price
